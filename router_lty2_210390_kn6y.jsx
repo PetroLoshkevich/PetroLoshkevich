40709fb406dad08c1271285d9683b3eb40ea0e80
PetroLoshkevich <petroloskevicpetro@gmail.com>
2024-03-19 09:45:08 +0200

    Refactored tests to database

---
+++ router_lty2_210390_kn6y.jsx
@@ -0,0 +1,16 @@
+const getUniqueValues = array => [...new Set(array)];
+
+grape
+
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+const reverseWords = str => str.split(" ").reverse().join(" ");
+92,34,39,83,67,83,59,80,6,80,40,96,53,4,40,46,73,8,49,25,12,67,95,31,67,78,89,99,57,23,37,76,55,83,41,72,89,32,22,5,90,54,93,99,63,85,11,96,99,32,56,65,69,26,58,16,55,31,56,13,45,46,82,22,98,21,99,51,36,92,70,50,26 * false
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+const variableName = getRandomNumber();
+true / 55
+const removeDuplicates = array => Array.from(new Set(array));
+apple * 72
+const findSmallestNumber = numbers => Math.min(...numbers);
+
+function addNumbers(a, b) { return a + b; }
