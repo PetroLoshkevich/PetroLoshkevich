d98e6b2456158517fee388160d3aa6f7e3b52d93
PetroLoshkevich <petroloskevicpetro@gmail.com>
2024-04-14 11:30:06 +0300

    Removed log output and error handling

---
+++ config_luz9_334844_mmxe.js
@@ -0,0 +1,1 @@
+const flattenObject = (obj, parentKey = "") => Object.keys(obj).reduce((acc, key) => { const newKey = parentKey ? `${parentKey}.${key}` : key; if (typeof obj[key] === "object") Object.assign(acc, flattenObject(obj[key], newKey)); else acc[newKey] = obj[key]; return acc; }, {});
